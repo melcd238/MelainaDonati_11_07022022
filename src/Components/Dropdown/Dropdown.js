618c43927990f61c178c74e45d435dbfc1dabfdd
melcd238 <melcd238@gmail.com>
2022-02-08 18:04:19 +0100

    onClick sur les dropdown

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from '../Dropdown/Dropdown.module.css';
-import arrowback from '../../Images/arrowBack.png'
+import arrowup from '../../Images/arrowUp.png';
+import arrowback from '../../Images/arrowBack.png';
 
 
 
 function Dropdown (props){
+    const [isOpen, setIsOpen] = useState(false);
+
       return(
+          <div className={classes.dropdonwContainer}>
           <div className={classes.dropdown}>
             <p className={classes.title}>{props.value}</p>
-            <div className={classes.arrow}><img src={arrowback} alt="fleche"></img></div>
+            <div className={classes.arrow}
+                 onClick={e => {setIsOpen(!isOpen)}}>
+
+                     {isOpen ? 
+                     <img src={arrowback} alt="fleche vers le bas"></img>
+                     :
+                     <img src={arrowup} alt="fleche vers le haut"></img>
+                    }
+             </div>
+          </div>
+            <div className={isOpen ? classes.contentContainer : classes.contentContainerClose}>
+                   <p className={classes.contentParaph}>{props.content}</p>
+            </div>
           </div>
       )
 }
--- src/Containers/APropos/APropos.js
+++ src/Containers/APropos/APropos.js
@@ -10,11 +10,32 @@ function APropos(props){
         <div className={classes.AProposContainer}>
          <Banner/>
          <div className={classes.dropdownList}>
-             <Dropdown value="Fiabilité"/>
-             <Dropdown value="Respect"/>
-             <Dropdown value="Service"/>
-             <Dropdown value="Responsabilité"/>
-         </div>
+             <Dropdown value="Fiabilité"
+                       content="Faire un voyage signifie généralement y consacrer du temps et de l'argent.
+                        Il est donc important de trouver un logement qui convienne parfaitement à vos besoins.
+                        Voici quelques conseils pour vous aider à voyager en toute confiance, en sachant que vous avez pris la bonne décision.
+                        Trouvez le logement qui vous convient:
+                               - Lisez les évaluations et les commentaires
+                               - Consultez les dispositifs de sécurité
+                               - Obtenez des réponses à vos questions
+                               - Communiquez et payez toujours sur Kasa
+                               - Effectuez un contrôle de sécurité
+                               - Faites des recherches concernant les alertes et précautions de voyage"/>
+ 
+             <Dropdown value="Respect"
+                       content="La bienveillance fait partie des valeurs fondatrices de Kasa.
+                        Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+             <Dropdown value="Service"
+                       content="Afin de contribuer au bon fonctionnement de Kasa et de couvrir les coûts de nos services,
+                        tels que l'assistance utilisateurs 24h/24, nous appliquons des frais de service lorsqu'une réservation est confirmée."/>
+             <Dropdown value="Responsabilité"
+                       content="Vous êtes dans l'obligation de suivre nos consignes, comme notre Charte d'hospitalité,
+                        et de vous assurer que vous respectez les lois et autres règles qui s'appliquent
+                        à votre situation particulière et à votre région, comme la Politique de non-discrimination.
+                        Il est important de vous assurer que vous avez le droit d'accueillir des voyageurs dans votre logement.
+                        Par exemple, des contrats, des lois ou autres règlements locaux peuvent présenter certaines restrictions. "/>
+                                             
+        </div>
         </div>
     )
 }
