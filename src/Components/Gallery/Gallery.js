7aaa728f73a9d83fc575422088a9c6005d73db32
melcd238 <melcd238@gmail.com>
2022-02-10 13:39:05 +0100

    mise en place condition d'affichage des fleches sur la gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from '../Gallery/Gallery.module.css';
 import right from '../../Images/right.svg';
 import left from '../../Images/left.svg';
@@ -8,10 +8,22 @@ import left from '../../Images/left.svg';
 function Gallery (props){
      // State
      const [currentPicture, setCurrentPicture]=useState(props.pictures[0]);
+     const [arrayNumber,setArrayNumber] = useState(false);
+
+     const checkArrayNumber = () =>{
+         if (props.pictures.length === 1){
+             setArrayNumber(true);
+         }
+     }
+     //ComponentDidMount
+     useEffect(()=>{
+        checkArrayNumber()
+  });
     
       // recup le numéro en cours de la photo affichée:
      let index = props.pictures.indexOf(currentPicture);
      let number = ( <span>{++index}</span>)
+    
     
 
      const HandlePrevPicture = (e) =>{
@@ -45,14 +57,16 @@ function Gallery (props){
 
     return(
         <div className={classes.gallery} style={{backgroundImage:  `url(${currentPicture})`}}>
+           
             <div>
-                <img src={left} alt="fleche gauche" style={{marginLeft : "22px",cursor:"pointer"}} onClick = {(e)=>HandlePrevPicture(e)}/>
+                {arrayNumber ? "" : <img src={left} alt="fleche gauche" style={{marginLeft : "22px",cursor:"pointer"}} onClick = {(e)=>HandlePrevPicture(e)}/> }
             </div>
+          
             <div className={classes.compteur}>
                {number}
               /{props.pictures.length}</div>
             <div>
-                <img src={right} alt="fleche gauche" style={{marginRight : "22px",cursor:"pointer" }} onClick = {(e)=> HandleNextPicture(e)} />
+               {arrayNumber ? "" : <img src={right} alt="fleche gauche" style={{marginRight : "22px",cursor:"pointer" }} onClick = {(e)=> HandleNextPicture(e)} /> }
             </div>
         </div>
     )
