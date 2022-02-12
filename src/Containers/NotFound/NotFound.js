2924e2db60183a23d6cf34cdc80cfe54b0265f53
melcd238 <melcd238@gmail.com>
2022-02-12 08:30:54 +0100

    responsive footer

--- src/Containers/NotFound/NotFound.js
+++ src/Containers/NotFound/NotFound.js
@@ -8,9 +8,9 @@ function NotFound (){
     return(
        <div className={classes.notFoundContainer}>
            <div>
-              <img src={logoNotFound} alt="erreur 404"></img>
+              <img className={classes.logoNotF} src={logoNotFound} alt="erreur 404"></img>
            </div>
-           <p className={classes.oups}>Oups! La page que vous demandez n'existe pas</p>
+           <p className={classes.oups}>Oups! La page que vous demandez n'existe pas.</p>
            <Link to="/" className={classes.link}>Retourner sur la page d'accueil</Link>
        </div>
     )
